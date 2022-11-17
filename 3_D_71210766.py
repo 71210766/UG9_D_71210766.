9d9af08796ca0f51afdb6b2e55c964c477a88aee
71210766 <93719122+71210766@users.noreply.github.com>
2022-11-17 16:34:31 +0700

    Add files via upload

---
+++ 3_D_71210766.py
@@ -0,0 +1,8 @@
+nama = input("nama anda : ")
+kuliah = input("masukkan mata kuliah : ")
+grup = input("masukkan grup")
+pisah = nama.split(" ")
+depan = pisah [0]
+belakang = pisah [1]
+print("Hallo", belakang, ",", depan)
+print(depan, "Telah bergabung dikelas", kuliah, "pada grup", grup)
